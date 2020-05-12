fa8920a24492c185bc8f6aa0245b70d4272e84ea
elhane <elhane2015@yandex.ru>
2020-05-13 02:10:49 +0500

    проверка по критерям, часть 1

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task("images", function() {
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
+    .pipe(webp({quality: 80}))
     .pipe(gulp.dest("build/img"));
 });
 
